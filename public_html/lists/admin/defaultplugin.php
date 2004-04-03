a6cc223c51cfe1cff5aecf854b3fe20976e5a248
Michiel Dethmers <git@phplist.com>
2004-04-03 21:57:00 +0000

    fixed bug default plugin not referring to correct plugin directory

--- public_html/lists/admin/defaultplugin.php
+++ public_html/lists/admin/defaultplugin.php
@@ -4,7 +4,7 @@ require_once "accesscheck.php";
 class phplistPlugin {
   var $name = "Default Plugin";
 
-  var $coderoot = "./plugins/defaultplugin/"; # coderoot relative to the phplist admin directory
+  var $coderoot = "./PLUGIN_ROOTDIR/defaultplugin/"; # coderoot relative to the phplist admin directory
 
   function phplistplugin() {
     # initialisation code
