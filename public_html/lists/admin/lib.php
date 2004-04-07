3b13559636fd699ca86fb11eb9ad64daa557c5a2
Michiel Dethmers <git@phplist.com>
2004-04-07 16:59:02 +0000

    *** empty log message ***

--- public_html/lists/admin/lib.php
+++ public_html/lists/admin/lib.php
@@ -29,7 +29,7 @@ if (!defined("FCKIMAGES_DIR")) define("FCKIMAGES_DIR","uploadimages");
 if (!defined("USE_MANUAL_TEXT_PART")) define("USE_MANUAL_TEXT_PART",0);
 if (!defined("ALLOW_NON_LIST_SUBSCRIBE")) define("ALLOW_NON_LIST_SUBSCRIBE",0);
 if (!defined("MAILQUEUE_BATCH_SIZE")) define("MAILQUEUE_BATCH_SIZE",0);
-if (!defined("MAILQUEUE_BATCH_PERIOD")) define("MAILQUEUE_BATCH_PERIOD",6);
+if (!defined("MAILQUEUE_BATCH_PERIOD")) define("MAILQUEUE_BATCH_PERIOD",3600);
 
 # keep it default to old behaviour for now
 if (!defined("USE_PREPARE")) define("USE_PREPARE",1);
