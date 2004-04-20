6073fe2a2ab3795db796cad41ffcbcc53026bbfa
Michiel Dethmers <git@phplist.com>
2004-04-20 12:44:00 +0000

    added about page

---
+++ public_html/lists/admin/about.php
@@ -0,0 +1,25 @@
+<div align=center>
+<table width="75%" border=1 cellspacing=5 cellpadding=5>
+<tr><td colspan="2" class="abouthead"><?=NAME?></td></tr>
+<tr><td width=50 valign=top>Powered by <a href="http://www.phplist.com" target="_blank">PHPlist</a>, version <?=VERSION?> <a href="http://www.phplist.com" target="_blank"><br/><br/><img src="../images/power-phplist.png" width=70 height=30 border=0></a></td>
+<td valign=top>
+PHPlist is licensed with the <a href="http://www.gnu.org/copyleft" target="_blank">GNU Public License (GPL)</a>.
+<br/>
+Copyright &copy; 2000-2004 <a href="http://tincan.co.uk" target="_blank">Tincan Ltd.</a>
+<br/>
+<br/>
+<b>Developers</b>:<ul>
+<li>Michiel Dethmers, Tincan Ltd</li>
+<li>Pete Ness, Nessoft LLC</li>
+</ul>
+<p>The developers wish to thank the many contributors to this system, who have helped out with bug reports, suggestions, donations, feature requests, sponsoring, translations and many other contributions.</p>
+
+<b>Portions of the system include</b>
+<ul>
+<li><a href="http://www.webbler.org" target="_blank">Webbler</a> code, by Michiel Dethmers
+<li>Onyx-RSS, by Ed Swindelles</li>
+<li><a href="http://www.fredck.com/FCKeditor/" target="_blank">FCKeditor</a>, by Frederico Caldeira Knabben and team</li>
+<li>the <a href="http://phpmailer.sourceforge.net/" target="_blank">phpmailer</a> class </li>
+<li>The html email class from, <a href="http://www.phpguru.org/" target="_blank">Richard Heyes</a></li>
+</tr></table>
+</div>
