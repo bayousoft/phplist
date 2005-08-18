e35d91a370a920d3612fd846aac215c01f6f4deb
Michiel Dethmers <git@phplist.com>
2005-08-18 18:45:35 +0000

    *** empty log message ***

--- public_html/lists/admin/dlusers.php
+++ public_html/lists/admin/dlusers.php
@@ -1,14 +1,54 @@
 <?php
 require_once dirname(__FILE__).'/accesscheck.php';
 
+# for now redirect to export
+
+# it would be good to rewrite this to export the user search selection
+# in the users page instead.
+
+Header("Location: ./?page=export");
+exit;
+
 include dirname(__FILE__) . "/structure.php";
+
+if (isset($_GET['list'])) {
+  $list = sprintf('%d',$_GET['list']);
+} else {
+  $list = 0;
+}
+
+$access = accessLevel('export');
+switch ($access) {
+  case 'owner':
+    $querytables = $GLOBALS['tables']['list'].' list ,'.$GLOBALS['tables']['user'].' user ,'.$GLOBALS['tables']['listuser'].' listuser ';
+    $subselect = ' and listuser.listid = list.id and listuser.userid = user.id and list.owner = ' . $_SESSION['logindetails']['id'];
+    $listselect_where = ' where owner = ' . $_SESSION['logindetails']['id'];
+    $listselect_and = ' and owner = ' . $_SESSION['logindetails']['id'];
+    break;
+  case 'all':
+    if ($list) {
+      $querytables = $GLOBALS['tables']['user'].' user'.', '.$GLOBALS['tables']['listuser'].' listuser';
+      $subselect = ' and listuser.userid = user.id ';
+    } else {
+      $querytables = $GLOBALS['tables']['user'].' user';
+      $subselect = '';
+    }
+    $listselect_where = '';
+    $listselect_and = '';
+    break;
+  case 'none':
+  default:
+    $querytables = $GLOBALS['tables']['user'].' user';
+    $subselect = ' and user.id = 0';
+    $listselect_where = ' where owner = 0';
+    $listselect_and = ' and owner = 0';
+    break;
+}
 
 if (!$list)
   $filename = $GLOBALS['I18N']->get('PHPList Users').' '.date("Y-M-d").'.csv';
 else {
-  $res = Sql_Query("select name from {$tables['list']} where id = $list");
-  $row = Sql_Fetch_Row($res);
-  $filename = $GLOBALS['I18N']->get('PHPList Users').' '.$GLOBALS['I18N']->get('on').' '.$row[0].' '.date("Y-M-d").'.csv';
+  $filename = $GLOBALS['I18N']->get('PHPList Users').' '.$GLOBALS['I18N']->get('on').' '.listName($list).' '.date("Y-M-d").'.csv';
 }
 ob_end_clean();
 header("Content-type: ".$GLOBALS["export_mimetype"]);
