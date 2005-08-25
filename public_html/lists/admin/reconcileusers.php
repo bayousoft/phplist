e588ae43570f6b1193f90a29efb2c95c69ef8797
Michiel Dethmers <git@phplist.com>
2005-08-25 13:56:39 +0000

    fixed #3910, entered value wasn't recorded when moving users in Reconcile

--- public_html/lists/admin/reconcileusers.php
+++ public_html/lists/admin/reconcileusers.php
@@ -103,7 +103,7 @@ function moveUser($userid) {
   global $tables;
   $newlist = $_GET["list"];
   Sql_Query(sprintf('delete from %s where userid = %d',$tables["listuser"],$userid));
-  Sql_Query(sprintf('insert into %s (userid,listid) values(%d,%d)',$tables["listuser"],$userid,$newlist));
+  Sql_Query(sprintf('insert into %s (userid,listid,entered) values(%d,%d,now())',$tables["listuser"],$userid,$newlist));
 }
 
 function addUniqID($userid) {
