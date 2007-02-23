19674cf012f620fe45ed0394f6f3974d740ddf49
Michiel Dethmers <git@phplist.com>
2007-02-23 00:26:18 +0000

    *** empty log message ***

--- public_html/lists/admin/reindex.php
+++ public_html/lists/admin/reindex.php
@@ -8,8 +8,9 @@ foreach ($DBstruct as $table => $columns) {
   print $table.'<br/>';
   foreach ($columns as $column => $definition) {
     if (strpos($column,'index') === 0) {
-      print "Adding index: $definition[0] to $column, <br/>";
+      print "Adding index: $definition[0] to $table, <br/>";
       flush();
+      # ignore errors, which are most likely that the index already exists
       Sql_Query(sprintf('alter table %s add index %s',$table,$definition[0]),1);
     }
   }
