bec0d1d9f3f8c43441b0feeec0a9c3e0d7de1e8c
Michiel Dethmers <git@phplist.com>
2007-02-23 00:23:57 +0000

    added option to also blacklist emails when mass unconfirming them

--- public_html/lists/admin/massunconfirm.php
+++ public_html/lists/admin/massunconfirm.php
@@ -9,20 +9,26 @@ if (!$_SESSION['logindetails']['superuser']) {
 if ($_POST['unsubscribe']) {
   $emails = explode("\n",$_POST['unsubscribe']);
   $count = 0;
-  $unsubbed = 0;
+  $unsubbed = $blacklisted = 0;
   foreach ($emails as $email) {
     $email = trim($email);
     $count++;
     Sql_Query(sprintf('update %s set confirmed = 0 where email = "%s"',$GLOBALS['tables']['user'],$email));
     $unsubbed += Sql_Affected_Rows();
+    if (!empty($_POST['blacklist'])) {
+      $blacklisted++;
+      addUserToBlackList($email,$GLOBALS['I18N']->get('Blacklisted by').' '.$_SESSION['logindetails']['adminname']);
+    }
+
   }
-  printf($GLOBALS['I18N']->get('All done, %d emails processed, %d emails marked unconfirmed<br/>'),$count,$unsubbed);
+  printf($GLOBALS['I18N']->get('All done, %d emails processed, %d emails marked unconfirmed, %d emails blacklisted<br/>'),$count,$unsubbed,$blacklisted);
   return;
 }
 ?>
 
 <form method=post action="">
 <h1><?php echo $GLOBALS['I18N']->get('Mass unconfirm email addresses')?></h1>
+<?php echo $GLOBALS['I18N']->get('Check to also add the emails to the blacklist')?>  <input type="checkbox" name="blacklist" value="1"></br/>
 <p><?php echo $GLOBALS['I18N']->get('Paste the emails to mark unconfirmed in this box, and click continue')?></p>
 <input type=submit name="go" value="<?php echo $GLOBALS['I18N']->get('Continue')?>"><br/>
 <textarea name="unsubscribe" rows=30 cols=40></textarea>
