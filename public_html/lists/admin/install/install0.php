12f08d1dc4031d3f86e42c77744285536e42ade0
Hernan Cisneros <hernan@tincan.co.uk>
2007-07-13 17:22:58 +0000

    *** empty log message ***

--- public_html/lists/admin/install/install0.php
+++ public_html/lists/admin/install/install0.php
@@ -1,19 +1,26 @@
 <?php
 
 //require('requiredvars.php');
+print $GLOBALS["I18N"]->get($_SESSION["printeable"]);
 
 if ($_SESSION['dbCreatedSuccesfully'] == FALSE) {
   $yourValue = $_SESSION;
-  printf('<div class="explain">%s</div>', $GLOBALS['strDbExplain']);
+  print $GLOBALS["I18N"]->get(sprintf('<table width=500><tr><td><div class="explain">%s</div></td></tr></table>', $GLOBALS['strDbExplain']));
 }
 else {
-  printf('<p>%s</p>',$GLOBALS["strPhplistDbCreation"]);
+  print $GLOBALS["I18N"]->get(sprintf('<p>%s</p>',$GLOBALS["strPhplistDbCreation"]));
 }
 
-printf('<table><tr><td>%s</td><td><input name="database_name" type="text" value="%s"></td></tr>
+print $GLOBALS["I18N"]->get(sprintf('<style type="text/css">table tr td input { float:right; }</style><table width=500><tr><td>%s</td><td><input name="database_name" type="text" value="%s"></td></tr>
 <tr><td>%s</td><td><input name="database_host" type="text" value="%s"></td></tr>
 <tr><td>%s</td><td><input name="database_user" type="text" value="%s"></td></tr>
 <tr><td>%s</td><td><input name="database_password" type="text" value="%s"></td></tr>
-<tr><td></td><td></td></tr></table>', $GLOBALS["strDbname"], $_SESSION['database_name'], $GLOBALS["strDbhost"], $_SESSION['database_host'], $GLOBALS["strDbuser"], $_SESSION['database_user'], $GLOBALS["strDbpass"], $_SESSION['database_password']);
+<tr><td></td><td></td></tr></table>', $GLOBALS["strDbname"], $_SESSION['database_name'], $GLOBALS["strDbhost"], $_SESSION['database_host'], $GLOBALS["strDbuser"], $_SESSION['database_user'], $GLOBALS["strDbpass"], $_SESSION['database_password']));
+
+print $GLOBALS["I18N"]->get(sprintf('<table width=500>
+<tr><td colspan=2><div class="explain">%s</div></td></tr>
+<tr><td>%s</td><td><input name="database_root_user" type="text" value="%s"></td></tr>
+<tr><td>%s</td><td><input name="database_root_pass" type="text" value="%s"></td></tr>
+</table>',$GLOBALS["strDbroot"],$GLOBALS["strDbrootuser"], $_SESSION['database_root_user'], $GLOBALS["strDbrootpass"], $_SESSION['database_root_pass']));
 
 ?>
