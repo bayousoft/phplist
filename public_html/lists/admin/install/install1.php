0213953f82e62897abb372eee90f85681701cb0e
Hernan Cisneros <hernan@tincan.co.uk>
2007-07-13 17:23:55 +0000

    install1 file - conflict resolved

--- public_html/lists/admin/install/install1.php
+++ public_html/lists/admin/install/install1.php
@@ -1,10 +1,12 @@
 <?php
 require('configvars.php');
 
-printf('<div class="explain">%s</div>', $GLOBALS['strExplainInstall1']);
+print $GLOBALS["I18N"]->get($_SESSION["printeable"]);
 
-print editVariable($generalVars,'name', 'text');
+print $GLOBALS["I18N"]->get(sprintf('<div class="explain">%s</div>', $GLOBALS['strExplainInstall1']));
 
-print editVariable($bouncesVars,'name', 'text');
+print $GLOBALS["I18N"]->get(editVariable($generalVars,'name', 'text'));
+
+print $GLOBALS["I18N"]->get(editVariable($bouncesVars,'name', 'text'));
 
 ?>
