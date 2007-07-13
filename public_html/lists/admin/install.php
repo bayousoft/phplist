47b1d94e069c6286e6f12dfd02381dbd00309cab
Hernan Cisneros <hernan@tincan.co.uk>
2007-07-13 17:30:14 +0000

    Several changes made, working with I18N class, option to step back, etc.

--- public_html/lists/admin/install.php
+++ public_html/lists/admin/install.php
@@ -5,22 +5,23 @@ ob_start();
 
 session_start();
 
-if (isset($_POST['page']) && $_POST['page']!== 'write_install') {
+if (isset($_POST['page'])/* && $_POST['page'] !== 'write_install'*/) {
   $Page = $_POST['page'];
-  header("Location: ./?page=$Page");
+#  header("Location: ./?page=$Page");
 }
 
 foreach ($_POST as $key => $val) {
   $_SESSION[$key] = $val;
 }
-//print_r($_SERVER);
+//print_r($_SESSION);
 require("../texts/english.inc");
-require("mysql.inc");
-//include('connect.php');
+include("../texts/".$_SESSION["language_module"]."");
+require("install/mysql.inc");
 require("install/steps-lib.php");
 include("install/header-install.inc");
 require("install/requiredvars.php");
-error_reporting(0);
+require("languages.php");
+error_reporting(63);
 
 ?>
 <div class="install_start wrong">
@@ -31,18 +32,18 @@ $listsDirPath = substr($_SERVER['PHP_SELF'], 0, -15);
 $configFilePath = "../config/config.php";
 if (!file_exists($configFilePath)) {
   if (!is_writable("../config")) {
-    printf('%s<hr>',$GLOBALS["strConfigIsNotAndDirNotWri"]);
+    print $GLOBALS["I18N"]->get(sprintf('%s<hr>',$GLOBALS["strConfigIsNotAndDirNotWri"]));
   }
   willNotContinue();
 }
 else {
   if (!is_writable($configFilePath)) {
-    printf('%s',$GLOBALS["strConfigIsNotWritable"]);
+    print $GLOBALS["I18N"]->get(sprintf('%s',$GLOBALS["strConfigIsNotWritable"]));
     willNotContinue();
   }
   if (filesize($configFilePath) > 1) {
 //    printf('<br><br>%s<hr><a href="/lists/admin/?page=addfeature">%s</a>',$GLOBALS["strConfigHasContent"], $GLOBALS['addFeature']);
-    printf('<br><br>%s',$GLOBALS["strConfigHasContent"]);
+    print $GLOBALS["I18N"]->get(sprintf('<br><br>%s',$GLOBALS["strConfigHasContent"]));
 //    require_once dirname(__FILE__).'/accesscheck.php';
     willNotContinue();
   }
@@ -53,13 +54,25 @@ else {
 <?php
 
 
-if (isset($_SESSION['page'])) {
-  $page = $_SESSION['page'];
-  preg_match("/([\w_]+)/",$page,$regs);
+if (!$_SESSION["history"]) {
+$_SESSION["history"] = array();
+}
+if ($_SESSION["page"]) {
+  array_push($_SESSION["history"],$_SESSION["page"]);
+  $_SESSION["history"] = array_unique($_SESSION["history"]);
+}
+
+
+if (isset($_SESSION["page"]) && in_array($_GET["page"], $_SESSION["history"])/* != FALSE*/) {
+  $getpage = sprintf("%s",$_GET["page"]);
+  $page = $_SESSION["page"]!=$getpage?$getpage:$_SESSION["page"];
+  if (preg_match("/([\w_]+)/",$page,$regs)) {
   $page = $regs[1];
+  }
   if (!is_file('install/'.$page.'.php') ) {
     $page = 'home';
   }
+#  print "<b>$page</b>";
   getNextPageForm($page);
 }
 
@@ -69,6 +82,13 @@ else {
 }
 
 checkScalarInt($_SESSION, $GLOBALS['requiredVars']);
+$_SESSION["printeable"] = '<table width=500><tr><td>';
+for ($i=0;$i<count($_SESSION["history"]);$i++) {
+  $_SESSION["printeable"] .= sprintf('<a href="./?page=%s">Step %s</a> >> ', $_SESSION["history"][$i], $i);
+  #$_SESSION["printeable"] .= $_SESSION["history"][$i].'';
+}
+$_SESSION["printeable"] .= '</td></tr></table>';
+
 
 ?>
 <?php
