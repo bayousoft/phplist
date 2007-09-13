da62a9f1253aedd96833c38e6474d72befa9e992
Bas Ovink <bas.ovink@tincan.co.uk>
2007-09-13 19:58:06 +0000

    dbg function shows all var types and recurses into arrays

--- public_html/lists/admin/connect.php
+++ public_html/lists/admin/connect.php
@@ -702,9 +702,33 @@ function Help($topic,$text = '?') {
 }
 
 
-if (!function_exists("dbg")) {
-  function dbg($msg) {
+//if (!function_exists("dbg")) {
+//  function dbg($msg) {
+//  }
+//}
+
+function dbg($variable, $description = 'Value', $table_started = 0) {
+ # WARNING recursive
+
+  if(!$table_started)
+    echo "<ul type='circle' style='border:1px solid #a0a0a0;padding-bottom:4px;padding-right:4px'>\n<li>{" . getenv("REQUEST_URI") . "} ";
+  echo "<i>$description</i>: ";
+  if(is_array($variable))
+  {
+    echo "(array)[".count($variable)."]";
+    echo "<ul type='circle' style='border:1px solid #a0a0a0;padding-bottom:4px;padding-right:4px'>\n";
+    foreach( $variable as $key => $value)
+    {
+      echo "<li>\"{$key}\" => ";
+      dbg( &$value, '', TRUE);
+      echo "</li>\n";
+    }
+    echo "</ul>\n";
   }
+  else
+    echo "(".gettype($variable).") '{$variable}'\n";
+  if(!$table_started)
+    echo "</li></ul>\n";
 }
 
 function PageData($id) {
