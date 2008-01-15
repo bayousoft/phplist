6e9a323e300dbf37c0f1e5d76b67c490314b0c23
Brian DeRocher <brian@derocher.org>
2008-01-15 23:01:29 +0000

    Reformat PHP for readability.

--- public_html/lists/admin/install/install0.php
+++ public_html/lists/admin/install/install0.php
@@ -11,11 +11,16 @@ print $GLOBALS["I18N"]->get($_SESSION["printeable"]);
 //   print $GLOBALS["I18N"]->get(sprintf('<p>%s</p>',$GLOBALS["strPhplistDbCreation"]));
 // }
 
-print $GLOBALS["I18N"]->get(sprintf('<style type="text/css">table tr td input { float:right; }</style><table width=500><tr><td>%s</td><td><input name="database_name" type="text" value="%s"></td></tr>
+print $GLOBALS["I18N"]->get(sprintf('<style type="text/css">table tr td input { float:right; }</style><table width=500>
+<tr><td>%s</td><td><input name="database_name" type="text" value="%s"></td></tr>
 <tr><td>%s</td><td><input name="database_host" type="text" value="%s"></td></tr>
 <tr><td>%s</td><td><input name="database_user" type="text" value="%s"></td></tr>
 <tr><td>%s</td><td><input name="database_password" type="text" value="%s"></td></tr>
-<tr><td></td><td></td></tr></table>', $GLOBALS["strDbname"], $_SESSION['database_name'], $GLOBALS["strDbhost"], $_SESSION['database_host'], $GLOBALS["strDbuser"], $_SESSION['database_user'], $GLOBALS["strDbpass"], $_SESSION['database_password']));
+<tr><td></td><td></td></tr></table>'
+, $GLOBALS["strDbname"], $_SESSION['database_name']
+, $GLOBALS["strDbhost"], $_SESSION['database_host']
+, $GLOBALS["strDbuser"], $_SESSION['database_user']
+, $GLOBALS["strDbpass"], $_SESSION['database_password']));
 
 print $GLOBALS["I18N"]->get(sprintf('<table width=500>
 <tr><td colspan=2><div class="explain">%s</div></td></tr>
