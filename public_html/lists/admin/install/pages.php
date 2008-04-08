50a9822267b16b03598248f752d6342d555ff5d8
Hernan Cisneros <hernan@tincan.co.uk>
2008-04-08 13:50:47 +0000

    No ADOdb for now, so only mysql is supported. All ADOdb statements have been removed

--- public_html/lists/admin/install/pages.php
+++ public_html/lists/admin/install/pages.php
@@ -16,16 +16,15 @@ switch ($actualPage) {
     
     print $GLOBALS["I18N"]->get(sprintf('<style type="text/css">table tr td input { float:right; }</style><table width=500><tr><td>%s</td><td><input name="database_name" type="text" value="%s"></td></tr>
     <tr><td>%s</td><td><input name="database_host" type="text" value="%s"></td></tr>
-    <tr><td>%s</td><td><input name="database_schema" type="text" value="%s"></td></tr>
     <tr><td>%s</td><td><input name="database_user" type="text" value="%s"></td></tr>
     <tr><td>%s</td><td><input name="database_password" type="text" value="%s"></td></tr>
     <tr><td></td><td></td></tr></table>'
 , $GLOBALS["strDbname"], $_SESSION['database_name']
 , $GLOBALS["strDbhost"], $_SESSION['database_host']
-, $GLOBALS["strDbschema"], $_SESSION['database_schema']
+//, $GLOBALS["strDbschema"], $_SESSION['database_schema']
 , $GLOBALS["strDbuser"], $_SESSION['database_user']
 , $GLOBALS["strDbpass"], $_SESSION['database_password']));
-    
+    // <tr><td>%s</td><td><input name="database_schema" type="text" value="%s"></td></tr>
     print $GLOBALS["I18N"]->get(sprintf('<table width=500>
     <tr><td colspan=2><div class="explain">%s</div></td></tr>
     <tr><td>%s</td><td><input name="database_root_user" type="text" value="%s"></td></tr>
@@ -34,7 +33,7 @@ switch ($actualPage) {
   break;
   case "install01":
     $editable = "";
-    if (isset($_SESSION['database_root_user']) && isset($_SESSION['database_root_pass'])) {
+    if (isset($_SESSION['database_root_user']) && isset($_SESSION['database_root_pass']) && !empty($_SESSION['database_root_user']) && !empty($_SESSION['database_root_pass'])) {
       $root_connection = Sql_Connect($_SESSION['database_host'], $_SESSION['database_root_user'], $_SESSION['database_root_pass'], $_SESSION['database_name']);
       if (!$root_connection) {
         $root_connection = Sql_Connect_Install($_SESSION['database_host'], $_SESSION['database_root_user'], $_SESSION['database_root_pass']);
@@ -55,11 +54,11 @@ switch ($actualPage) {
     sleep(2);
     #flush();
     $test_connection = Sql_Connect($_SESSION['database_host'], $_SESSION['database_user'], $_SESSION['database_password'], $_SESSION['database_name']);
-    Sql_Set_Search_Path($_SESSION['database_schema']);
+//    if ($GLOBALS['adodb_connection']) Sql_Set_Search_Path($_SESSION['database_schema']);
     
     if ($test_connection) {
     $GLOBALS["database_connection"] = $test_connection;
-    include("install/dbchecking.php");
+    #include("install/dbchecking.php"); not fully tested yet
     }
     
     if (!$test_connection) { // let's connect without a db
@@ -68,17 +67,17 @@ switch ($actualPage) {
         $GLOBALS["database_connection"] = $test_connection2;
         $create_db = Sql_Create_Db($_SESSION['database_name']);
         if (!$create_db) {
-          $errorno = sql_errorno();
+          $errorno = Sql_has_error();
           $procedure = 2;
         } else {
           $procedure = 1;
         }
       } else {
-        $errorno = sql_errorno();
+        $errorno = Sql_has_error();
         $procedure = 3;
       }
     } else {
-      $errorno = sql_errorno();
+      $errorno = Sql_has_error();
       $procedure = $errorno ? 2 : 1;
     }
     
