0f002169495224db2106c2c2bd41613323cdd1e2
Michiel Dethmers <git@phplist.com>
2009-10-15 12:07:58 +0000

    fixed merge error

--- public_html/lists/admin/lan/nl/configure.php
+++ public_html/lists/admin/lan/nl/configure.php
@@ -1,13 +1,4 @@
-<<<<<<< .working
-<?php
-$lan = array(
- "checklist for installation" => "Checklist voor Installatie",
- "invalid request" => "ongeldig verzoek",
- "cannot be empty" => "kan niet leeg zijn",
- "editing" => "Bewerken",
- "save changes" => "Wijzigingen opslaan",
- "edit" => "bewerk",
-?>=======
+
 <?php
 $lan = array(
  "checklist for installation" => "Checklist voor Installatie",
