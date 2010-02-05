4b697eb53a1af835caeea919185982c165a7789b
Pablo Armelin <pablo@tincan.co.uk>
2010-02-05 16:34:13 +0000

    Many small validation changes

--- public_html/lists/admin/lan/de/admin.php
+++ public_html/lists/admin/lan/de/admin.php
@@ -8,10 +8,10 @@ $lan = array(
   'Error adding new admin' => 'Fehler beim Hinzuf&uuml;gen eines neuen Administrators',
   'strAccessExplain' => '<hr/>
 <h4>Zugriffsrechte</h4>
-<p>Rechte eines Administrators beim Zugriff auf die einzelnen Seiten im System</p>
+<p class="information">Rechte eines Administrators beim Zugriff auf die einzelnen Seiten im System</p>
 <ul>
   <li><strong>Alle:</strong> Der Administrator hat uneingeschr&auml;nkten Zugriff auf die Seite (lesen und schreiben).</li>
-  <li><strong>Lesen:</strong> Der Administrator kann die Seite sehen, aber keine &Auml;nderungen daran vornehmen.
+  <li><strong>Lesen:</strong> Der Administrator kann die Seite sehen, aber keine &Auml;nderungen daran vornehmen
   	Diese Einstellung funktioniert derzeit erst f&uuml;r die Seiten "user", "users" und "members".</li>
   <li><strong>Keine:</strong> Der Administrator kann die Seite nicht sehen.</li>
   <li><strong>Eigene:</strong> Der Administrator kann die Seite sehen, allerdings nur den Inhalt seiner eigenen Listen.</li>
