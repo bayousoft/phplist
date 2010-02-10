2b47410571aded27e7fde23a025f9fa4b16dce51
Pablo Armelin <pablo@tincan.co.uk>
2010-02-10 17:07:51 +0000

    replaced a few p tags with divs for easier formatting

--- public_html/lists/admin/configure.php
+++ public_html/lists/admin/configure.php
@@ -45,14 +45,14 @@ if (!$id) {
         $value = $dbval;
       else
         $value = $val[0];
-      printf('<p class="configEdit"><a href="%s">%s</a> <b>%s</b><br/>',PageURL2("configure","","id=$key"),$GLOBALS['I18N']->get('edit'),$GLOBALS['I18N']->get($val[1]));
-      print nl2br(htmlspecialchars(stripslashes($value))) . "<br/><hr/>";
+      printf('<div class="configEdit"><a href="%s">%s</a> <b>%s</b>',PageURL2("configure","","id=$key"),$GLOBALS['I18N']->get('edit'),$GLOBALS['I18N']->get($val[1]));
+      print nl2br(htmlspecialchars(stripslashes($value))) . "</div>";
     }
   }
 } else {
   $val = $default_config[$id];
-  printf('%s<p class="configEditing">' . $GLOBALS['I18N']->get('editing') . ' <b>%s</b></p>',formStart(' class="configForm" '),$GLOBALS['I18N']->get($val[1]));
-  printf('<input type="hidden" name="id" value="%s">',$id);
+  printf('%s<div class="configEditing">' . $GLOBALS['I18N']->get('editing') . ' <b>%s</b></div>',formStart(' class="configForm" '),$GLOBALS['I18N']->get($val[1]));
+  printf('<div class="configValue"><input type="hidden" name="id" value="%s" />',$id);
   $dbval = getConfig($id);
 #  print $dbval.'<br/>';
   if (isset($dbval))
@@ -66,14 +66,14 @@ if (!$id) {
   }
 #  print $value . '<br/>';
   if ($val[2] == "textarea")
-    printf('<textarea name="values[%s]" rows=25 cols=55>%s</textarea><br/>',
+    printf('<textarea name="values[%s]" rows=25 cols=55>%s</textarea>',
       $id,htmlspecialchars(stripslashes($value)));
   else if ($val[2] == "text")
-    printf('<input type="text" name="values[%s]" size="70" value="%s"><br/>',
+    printf('<input type="text" name="values[%s]" size="70" value="%s" />',
     $id,htmlspecialchars(stripslashes($value)));
   else if ($val[2] == "boolean")
-    printf('<input type="text" name="values[%s]" size="10" value="%s"><br/>',
+    printf('<input type="text" name="values[%s]" size="10" value="%s" />',
     $id,htmlspecialchars(stripslashes($value)));
-  print '<br/><input type="hidden" name="save" value="1"><p class="submit"><input type="submit" name="savebutton" value="' . $GLOBALS['I18N']->get('save changes') . '"></p></form>';
+  print '</div><input type="hidden" name="save" value="1" /><input class="submit" type="submit" name="savebutton" value="' . $GLOBALS['I18N']->get('save changes') . '" /></form>';
 }
 ?>
