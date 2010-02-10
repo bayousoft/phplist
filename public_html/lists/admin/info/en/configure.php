f77da72a455777d553eba70d4a582feccb609d86
Pablo Armelin <pablo@tincan.co.uk>
2010-02-10 17:03:06 +0000

    Added div class=placeholders

--- public_html/lists/admin/info/en/configure.php
+++ public_html/lists/admin/info/en/configure.php
@@ -2,6 +2,8 @@
 You can use certain <b>Placeholders</b> in your values.
 Placeholders look like <b>[SOMETEXT]</b>, where <i>SOMETEXT</i> can be different.
 Some useful placeholders are:
+</p>
+<div class="placeholders">
 <ul>
 <li>WEBSITE - the address you type for your website</li>
 <li>DOMAIN - the text you type for your domain</li>
@@ -11,7 +13,7 @@ Some useful placeholders are:
 <li>PREFERENCESURL - the location of the page where users can update their details</li>
 <li>CONFIRMATIONURL - the location of the page where users have to confirm their subscription</li>
 </ul>
+</div>
 <!--For the header and the footer you can use the following code to include external documents:
 <br/>
 <b>[URL:&lt;Full URL of page to load&gt;]</b>-->
-</p>
