3687d43a31c2dc5d9ad71e70b470806b887a8175
Michiel Dethmers <git@phplist.com>
2010-04-05 19:51:26 +0000

    update to output of message status

--- public_html/lists/admin/msgstatus.php
+++ public_html/lists/admin/msgstatus.php
@@ -17,9 +17,15 @@ if ($message['status'] != 'inprocess') {
   if ($active > 60) {
     $html = $GLOBALS['I18N']->get('Stalled');
   } else {
+    $totalsent = $messagedata['astext'] + 
+      $messagedata['ashtml'] + 
+      $messagedata['astextandhtml'] + 
+      $messagedata['aspdf'] + 
+      $messagedata['astextandpdf'];
     $html = $GLOBALS['I18N']->get($message['status']).'<br/>'.
     $messagedata['to process'].' '.$GLOBALS['I18N']->get('still to process').'<br/>'.
     $GLOBALS['I18N']->get('ETA').': '.$messagedata['ETA'].'<br/>'.
+    $GLOBALS['I18N']->get('sent').': '.$totalsent.'<br/>'.
     $GLOBALS['I18N']->get('Processing').' '.sprintf('%d',$messagedata['msg/hr']).' '.$GLOBALS['I18N']->get('msgs/hr')
     ;
   }
