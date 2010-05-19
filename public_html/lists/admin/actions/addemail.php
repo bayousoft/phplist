7afb7f0e12f5f570e656fa1e46f315840e71c756
Michiel Dethmers <git@phplist.com>
2010-05-19 16:06:48 +0000

    long tags, always forget them

--- public_html/lists/admin/actions/addemail.php
+++ public_html/lists/admin/actions/addemail.php
@@ -1,7 +1,7 @@
-<?
+<?php
 
 if (!empty($_GET['email'])) {
-#  Sql_Query(sprintf('insert into %s (email) values("%s")',$GLOBALS['tables']['user'],sql_escape($_GET['email'])),1);
+  Sql_Query(sprintf('insert into %s (email) values("%s")',$GLOBALS['tables']['user'],sql_escape($_GET['email'])),1);
   addUserHistory($_GET['email'],'Added by '.adminName());
 }
 $status = $GLOBALS['I18N']->get('Email added');
