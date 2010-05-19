5a0017e76a84b2b2e6a27f668ccebb8c8aeec78a
Michiel Dethmers <git@phplist.com>
2010-05-19 16:08:13 +0000

    show description instead of key

--- public_html/lists/admin/configure.php
+++ public_html/lists/admin/configure.php
@@ -3,7 +3,6 @@
 
 <?php
 require_once dirname(__FILE__).'/accesscheck.php';
-
 /*
 if ($_GET["firstinstall"] || $_SESSION["firstinstall"]) {
   $_SESSION["firstinstall"] = 1;
@@ -30,9 +29,9 @@ if (!empty($_REQUEST['save']) && $id) {
       $_POST["values"][$id] = str_replace("[DOMAIN]","",$_POST["values"][$id]);
       $_POST["values"][$id] = str_replace("[WEBSITE]","",$_POST["values"][$id]);
     }
-    if ($_POST["values"][$id] == "" && !$info[3])
+    if ($_POST["values"][$id] == "" && !$info[3]) {
       Error("$info[1] " . $GLOBALS['I18N']->get('cannot be empty'));
-    else {
+    } else {
       SaveConfig($id,$_POST["values"][$id],0);
       Redirect("configure");
       exit;
@@ -48,7 +47,7 @@ if (!$id) {
         $value = $dbval;
       else
         $value = $val[0];
-      printf('<div class="configEdit"><a href="%s" class="ajaxable">%s</a> <b>%s</b></div>',PageURL2("configure","","id=$key"),$GLOBALS['I18N']->get('edit'),$key,$GLOBALS['I18N']->get($val[1]));
+      printf('<div class="configEdit"><a href="%s" class="ajaxable">%s</a> <b>%s</b></div>',PageURL2("configure","","id=$key"),$GLOBALS['I18N']->get('edit'),$GLOBALS['I18N']->get($val[1]));
       printf('<div id="edit_%s" class="configcontent">'.nl2br(htmlspecialchars(stripslashes($value))) . '</div></div>',$key);
     }
   }
