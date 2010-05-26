3914109e70e3b3608ffa0f8f5ebeb50f223321bf
Michiel Dethmers <git@phplist.com>
2010-05-26 18:44:33 +0000

    js and css updates

--- public_html/lists/admin/js/phplist.js
+++ public_html/lists/admin/js/phplist.js
@@ -11,7 +11,7 @@ function urlParameter( name, link) {
 }
 
 function messageStatusUpdate(msgid) {
-   $('#messagestatus'+msgid).load('./?page=ajaxcall&action=msgstatus&id='+msgid,"",function() {
+   $('#messagestatus'+msgid).load('./?page=pageaction&ajaxed=true&action=msgstatus&id='+msgid,"",function() {
    });
    setTimeout("messageStatusUpdate("+msgid+")",5000);
 }
@@ -22,21 +22,15 @@ function openDialog(url) {
     width: 600,
     modal: true,
     show: 'blind',
-    hide: 'explode',
-    // this doesn't seem to work
-    //buttons: {
-      //Ok: function() {
-        //$(this).dialog('close');
-      //}
-    //}
+    hide: 'explode'
   });
   var destpage = urlParameter('page',url);
-  var url = url.replace(/page=/,'origpage=');
+  url = url.replace(/page=/,'origpage=');
   $("#dialog").load(url+'&ajaxed=true&page=pageaction&action='+destpage);
 }
 
 function totalSentUpdate(msgid) {
-   $('#totalsent'+msgid).load('./?page=ajaxcall&action=msgsent&id='+msgid,"",function() {
+   $('#totalsent'+msgid).load('./?page=pageaction&ajaxed=true&action=msgsent&id='+msgid,"",function() {
    });
    setTimeout("totalSentUpdate("+msgid+")",5000);
 }
@@ -55,10 +49,12 @@ $("a.ajaxable").click(function() {
   var thispage = urlParameter('page',window.location.href);
   var action = urlParameter('action',url);
   if (action == "") {
-    action = thispage;
+    url += '&action='+thispage;
   }
   parent = $(this).parent();
-  parent.load(url+'&ajaxed=true&page=pageaction&action='+action);
+  url = url.replace(/page=/,'origpage=');
+  //alert(url+'&ajaxed=true&page=pageaction');
+  parent.load(url+'&ajaxed=true&page=pageaction');
   return false;
 })
 
@@ -79,7 +75,7 @@ $("input:checkbox.checkallcheckboxes").click(function() {
 $(".accordion").accordion({
     autoHeight: false,
     navigation: true,
-    collapsible: true,
+    collapsible: true
   })
 
 $(".opendialog").click(function() {
@@ -91,7 +87,6 @@ $(".helpdialog").click(function() {
   return false;
 });
 $(".closedialog").click(function() {
-  alert("CLose");
   $("#dialog").dialog('close');
 });
 
@@ -118,71 +113,26 @@ $("#emailsearch").autocomplete({
     }
   });
 
+  $("#remoteurlinput").blur(function() {
+    $("#remoteurlstatus").load("./?page=pageaction&action=checkurl&ajaxed=true&url="+this.value);
+  })
 
+  $("input:radio[name=sendmethod]").change(function() {
+    if (this.value == "remoteurl") {
+      $("#remoteurl").show();
+      $("#messagecontent").hide();
+    } else {
+      $("#remoteurl").hide();
+      $("#messagecontent").show();
+    }
+  })
 
-		//scrollpane parts
-		var scrollPane = $('.scroll-pane');
-		var scrollContent = $('.scroll-content');
-		
-		//build slider
-		var scrollbar = $(".scroll-bar").slider({
-			slide:function(e, ui){
-				if( scrollContent.width() > scrollPane.width() ){ scrollContent.css('margin-left', Math.round( ui.value / 100 * ( scrollPane.width() - scrollContent.width() )) + 'px'); }
-				else { scrollContent.css('margin-left', 0); }
-			}
-		});
-		
-		//append icon to handle
-		var handleHelper = scrollbar.find('.ui-slider-handle')
-		.mousedown(function(){
-			scrollbar.width( handleHelper.width() );
-		})
-		.mouseup(function(){
-			scrollbar.width( '100%' );
-		})
-		.append('<span class="ui-icon ui-icon-grip-dotted-vertical"></span>')
-		.wrap('<div class="ui-handle-helper-parent"></div>').parent();
-		
-		//change overflow to hidden now that slider handles the scrolling
-		scrollPane.css('overflow','hidden');
-		
-		//size scrollbar and handle proportionally to scroll distance
-		function sizeScrollbar(){
-			var remainder = scrollContent.width() - scrollPane.width();
-			var proportion = remainder / scrollContent.width();
-			var handleSize = scrollPane.width() - (proportion * scrollPane.width());
-			scrollbar.find('.ui-slider-handle').css({
-				width: handleSize,
-				'margin-left': -handleSize/2
-			});
-			handleHelper.width('').width( scrollbar.width() - handleSize);
-		}
-		
-		//reset slider value based on scroll content position
-		function resetValue(){
-			var remainder = scrollPane.width() - scrollContent.width();
-			var leftVal = scrollContent.css('margin-left') == 'auto' ? 0 : parseInt(scrollContent.css('margin-left'));
-			var percentage = Math.round(leftVal / remainder * 100);
-			scrollbar.slider("value", percentage);
-		}
-		//if the slider is 100% and window gets larger, reveal content
-		function reflowContent(){
-				var showing = scrollContent.width() + parseInt( scrollContent.css('margin-left') );
-				var gap = scrollPane.width() - showing;
-				if(gap > 0){
-					scrollContent.css('margin-left', parseInt( scrollContent.css('margin-left') ) + gap);
-				}
-		}
-		
-		//change handle position on window resize
-		$(window)
-		.resize(function(){
-				resetValue();
-				sizeScrollbar();
-				reflowContent();
-		});
-		//init scrollbar size
-		setTimeout(sizeScrollbar,10);//safari wants a timeout
-
+  $("a.savechanges").click(function() {
+    if (changed) {
+      document.sendmessageform.followupto.value = this.href;
+      document.sendmessageform.submit();
+      return false;
+    }
+  });
 
 })
