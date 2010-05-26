e44107a71eaa9b64082c602548eda90dfabd87aa
Michiel Dethmers <git@phplist.com>
2010-05-26 18:43:46 +0000

    xhtml endings

--- public_html/lists/admin/pagetop.php
+++ public_html/lists/admin/pagetop.php
@@ -11,18 +11,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" >
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
 <head>
-<meta http-equiv="pragma" content="no-cache">
-<meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
-<link rev="made" href="mailto:phplist%40tincan.co.uk">
-<link rel="home" href="http://www.phplist.com" title="phplist homepage">
-<link rel="copyright" href="http://tincan.co.uk" title="Copyright">
-<link rel="license" href="http://www.gnu.org/copyleft/gpl.html" title="GNU General Public License">
-<meta name="Author" content="Michiel Dethmers - http://www.phplist.com">
-<meta name="Copyright" content="Michiel Dethmers, Tincan Ltd - http://tincan.co.uk">
-<meta name="Powered-By" content="phplist version <?php echo VERSION?>">
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $strCharSet?>">
-<link rel="SHORTCUT ICON" href="./images/phplist.ico">
-<link href="styles/phplist.css" type="text/css" rel="stylesheet">
+<meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="Cache-Control" content="no-cache, must-revalidate" />
+<link rev="made" href="mailto:phplist%40tincan.co.uk" />
+<link rel="home" href="http://www.phplist.com" title="phplist homepage" />
+<link rel="copyright" href="http://tincan.co.uk" title="Copyright" />
+<link rel="license" href="http://www.gnu.org/copyleft/gpl.html" title="GNU General Public License" />
+<meta name="Author" content="Michiel Dethmers - http://www.phplist.com" />
+<meta name="Copyright" content="Michiel Dethmers, Tincan Ltd - http://tincan.co.uk" />
+<meta name="Powered-By" content="phplist version <?php echo VERSION?>" />
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $strCharSet?>" />
+<link rel="SHORTCUT ICON" href="./images/phplist.ico" />
+<link href="styles/phplist.css" type="text/css" rel="stylesheet" />
 <link href="styles/jquery-ui-1.8.1.all.css" rel="stylesheet" type="text/css"/>
 <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.1.all.min.js"></script>
