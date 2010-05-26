bdac3220841e7649b1c8082a219c276bf091d4f3
Michiel Dethmers <git@phplist.com>
2010-05-26 18:51:12 +0000

    add some more values when adding an email

--- public_html/lists/admin/actions/addemail.php
+++ public_html/lists/admin/actions/addemail.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!empty($_GET['email'])) {
-  Sql_Query(sprintf('insert into %s (email) values("%s")',$GLOBALS['tables']['user'],sql_escape($_GET['email'])),1);
+  Sql_Query(sprintf('insert into %s (email,uniqid,htmlemail,entered) values("%s","%s",1,now())',$GLOBALS['tables']['user'],sql_escape($_GET['email']),getUniqid()),1);
   addUserHistory($_GET['email'],'Added by '.adminName(),'');
 }
 $status = $GLOBALS['I18N']->get('Email added');
