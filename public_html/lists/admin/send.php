146ac13aa2e1be4edb948e603d092e1258f4e15f
Michiel Dethmers <git@phplist.com>
2010-05-26 18:43:12 +0000

    organise lists by categories in tabs

--- public_html/lists/admin/send.php
+++ public_html/lists/admin/send.php
@@ -123,79 +123,53 @@ if ($done) {
 }
 */
 $list_content = '
-<p class="button">'.$GLOBALS['I18N']->get('selectlists').':</p>
-<ul>
-<li><input type="checkbox" name="targetlist[all]"';
-  if (isset($_POST["targetlist"]["all"]) && $_POST["targetlist"]["all"])
-    $list_content .= "checked";
-$list_content .= ' />'.$GLOBALS['I18N']->get('alllists').'</li>';
+<div id="listselection" class="accordion">
+<h3><a name="lists">'.$GLOBALS['I18N']->get('selectlists').':</a></h3>
+';
 
-$list_content .= '<li><input type="checkbox" name="targetlist[allactive]"';
-  if (isset($_POST["targetlist"]["allactive"]) && $_POST["targetlist"]["allactive"])
-    $list_content .= 'checked="checked"';
-$list_content .= ' />'.$GLOBALS['I18N']->get('All Active Lists').'</li>';
+$listindex = $listhtml = '';
 
-$result = Sql_query("SELECT * FROM $tables[list] $subselect order by name");
-while ($row = Sql_fetch_array($result)) {
-  # check whether this message has been marked to send to a list (when editing)
-  $checked = 0;
-  if ($_GET["id"]) {
-    $sendtolist = Sql_Query(sprintf('select * from %s where
-      messageid = %d and listid = %d',$tables["listmessage"],$_GET["id"],$row["id"]));
-    $checked = Sql_Num_Rows($sendtolist);
-  }
-  $list_content .= sprintf('<li><input type="checkbox" name="targetlist[%d]" value="%d" ',$row["id"],$row["id"]);
-  if ($checked || (isset($_POST["targetlist"][$row["id"]]) && $_POST["targetlist"][$row["id"]]))
-    $list_content .= 'checked="checked"';
-  $list_content .= " />".stripslashes($row["name"]);
-  if ($row["active"])
-    $list_content .= ' ('.$GLOBALS['I18N']->get('listactive').')';
-  else
-    $list_content .= ' ('.$GLOBALS['I18N']->get('listnotactive').')';
+$categoryhtml = ListofLists($messagedata,'targetlist',$subselect);
 
-  $desc = nl2br(stripslashes($row["description"]));
-
-  $list_content .= "<br/>$desc</li>";
-  $some = 1;
+$tabno = 1;
+foreach ($categoryhtml as $category => $content) {
+  $listindex .= sprintf('<li><a href="#include%d">%s</a></li>',$tabno,$category);
+  $listhtml .= sprintf('<div id="include%d"><ul>%s</ul></div>',$tabno,$content);
+  $tabno++;
 }
-$list_content .= '</ul>';
+
+$list_content .= '<div class="tabbed"><ul>'.$listindex.'</ul>';
+$list_content .= $listhtml;
+
+$list_content .= '</div>';
 
 if (USE_LIST_EXCLUDE) {
   $list_content .= '
-    <hr/><h3>'.$GLOBALS['I18N']->get('selectexcludelist').'</h3><p class="button">'.$GLOBALS['I18N']->get('excludelistexplain').'</p>
-    <ul>';
+    <h3><a name="excludelists">'.$GLOBALS['I18N']->get('selectexcludelist').'</a></h3><div><p class="button">'.$GLOBALS['I18N']->get('excludelistexplain').'</p>';
+  $categoryhtml = ListofLists($messagedata,'excludelist',$subselect);
+  $tabno = 1;
+  $listindex = $listhtml = '';
 
-  $dbdata = Sql_Fetch_Row_Query(sprintf('select data from %s where name = "excludelist" and id = %d',
-    $GLOBALS["tables"]["messagedata"],$_GET["id"]));
-  $excluded_lists = explode(",",$dbdata[0]);
-
-  $result = Sql_query(sprintf('SELECT * FROM %s %s order by name',$GLOBALS["tables"]["list"],$subselect));
-  while ($row = Sql_fetch_array($result)) {
-    $checked = in_array($row["id"],$excluded_lists);
-    $list_content .= sprintf('<li><input type="checkbox" name="excludelist[%d]" value="%d" ',$row["id"],$row["id"]);
-    if ($checked || isset($_POST["excludelist"][$row["id"]]))
-      $list_content .= 'checked="checked"';
-    $list_content .= " />".stripslashes($row["name"]);
-    if ($row["active"])
-      $list_content .= ' ('.$GLOBALS['I18N']->get('listactive').')';
-    else
-      $list_content .= ' ('.$GLOBALS['I18N']->get('listnotactive').')';
-
-    $desc = nl2br(stripslashes($row["description"]));
-
-    $list_content .= "<br/>$desc</li>";
+  foreach ($categoryhtml as $category => $content) {
+    if ($category != 'all') {
+      $listindex .= sprintf('<li><a href="#exclude%d">%s</a></li>',$tabno,$category);
+      $listhtml .= sprintf('<div id="exclude%d"><ul>%s</ul></div>',$tabno,$content);
+      $tabno++;
+    }
   }
-  $list_content .= '</ul>';
+
+  $list_content .= '<div class="tabbed"><ul>'.$listindex.'</ul>';
+  $list_content .= $listhtml;
+
+  $list_content .= '</div>';
+
 }
 
-if (!$some)
+$list_content .= '</div></div>'; ## close accordion
+
+if (!$some) {
   $list_content = $GLOBALS['I18N']->get('nolistsavailable');
-
-$list_content .= '
-
-<input class="submit" type="submit" name="send" value="'.$GLOBALS['I18N']->get('sendmessage').'" />
-
-';
+}
 
 if (isset($show_lists) && $show_lists) {
   print $list_content;
