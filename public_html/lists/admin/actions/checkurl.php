91831a34f620beb772cba536d9841236109c3fd0
Michiel Dethmers <git@phplist.com>
2010-06-22 13:22:55 +0000

    include the URL expansion config when checking for validity of a URL

--- public_html/lists/admin/actions/checkurl.php
+++ public_html/lists/admin/actions/checkurl.php
@@ -6,12 +6,21 @@ $request_parameters = array(
   'allowRedirects' => 1,
   'method' => 'HEAD',
 );
-$headreq = new HTTP_Request($_GET['url'],$request_parameters);
+
+if (empty($_GET['url'])) return;
+
+$url = expandURL($_GET['url']);
+
+$headreq = new HTTP_Request($url,$request_parameters);
 $headreq->addHeader('User-Agent', 'phplist v'.VERSION.' (http://www.phplist.com)');
 if (!PEAR::isError($headreq->sendRequest(false))) {
   $code = $headreq->getResponseCode();
   if ($code != 200) {
-    $status = $GLOBALS['I18N']->get('Error fetching URL');
+    if (!empty($url_append)) {
+      $status = $GLOBALS['I18N']->get('Error fetching URL').' '.$GLOBALS['I18N']->get('Check your "remoteurl_append" setting.');
+    } else {
+      $status = $GLOBALS['I18N']->get('Error fetching URL');
+    }
     return;
   }
 } else {
