8981189d62cb8262a8487cedf33e48de9364b885
Michiel Dethmers <git@phplist.com>
2010-06-23 18:08:09 +0000

    update short to long php tags script

--- scripts/short2long.sh
+++ scripts/short2long.sh
@@ -3,18 +3,23 @@
 
 echo "replacing <? with <?php"
 for i in `fgrep -rilI -e "<?" --exclude-dir .svn public_html*`; do
-#   echo $i;
-   sed -i "s/<?/<?php/" $i;
+   echo $i;
+   sed -i "s/<? /<?php /" $i;
 done
 # remove accidentally doubled ones
 echo "replacing <?phpphp with <?php"
 for i in `fgrep -rilI -e "<?phpphp" --exclude-dir .svn public_html*`; do
-#   echo $i;
+   echo $i;
    sed -i "s/<?phpphp/<?php/" $i;
 done
+echo "replacing <?= with <?php echo "
+for i in `fgrep -rilI -e "<?=" --exclude-dir .svn public_html*`; do
+   echo $i;
+   sed -i "s/<?=/<?php echo /" $i;
+done
 echo "replacing <?php= with <?php echo "
-for i in `fgrep -rilI -e "<?php= " --exclude-dir .svn public_html*`; do
-#   echo $i;
+for i in `fgrep -rilI -e "<?php=" --exclude-dir .svn public_html*`; do
+   echo $i;
    sed -i "s/<?php=/<?php echo /" $i;
 done
 
