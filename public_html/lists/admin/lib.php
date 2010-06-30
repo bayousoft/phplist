227d6e01c33b592585aebafff352a29423b0aeb2
Michiel Dethmers <git@phplist.com>
2010-06-30 15:44:53 +0000

    always include phplistmailer class, so that system messages are sent using it

--- public_html/lists/admin/lib.php
+++ public_html/lists/admin/lib.php
@@ -3,6 +3,7 @@ require_once dirname(__FILE__)."/accesscheck.php";
 # library used for plugging into the webbler, instead of "connect"
 # depricated and should be removed
 #error_reporting(63);
+include_once dirname(__FILE__).'/class.phplistmailer.php';
 
 $domain = getConfig("domain");
 $website = getConfig("website");
