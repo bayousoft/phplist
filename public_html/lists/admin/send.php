e95ef967cb5a4998ba7db1458a32f514c712f5f1
Michiel Dethmers <git@phplist.com>
2010-07-07 13:51:53 +0000

    bit more whitespace

--- public_html/lists/admin/send.php
+++ public_html/lists/admin/send.php
@@ -85,12 +85,11 @@ if (!empty($_GET['delete'])) {
   }
 }
 
-
 $req = Sql_Query(sprintf('select id,entered,subject,unix_timestamp(current_timestamp) - unix_timestamp(entered) as age from %s where status = "draft" %s order by entered desc',$GLOBALS['tables']['message'],$ownership));
 $numdraft = Sql_Num_Rows($req);
 if ($numdraft > 0 && !isset($_GET['id']) && !isset($_GET['new'])) {
   print '<p class="button">'.PageLink2('send&amp;new=1',$I18N->get('start a new message')).'</p>';
-  print '<p><h3>'.$I18N->get('Choose an existing draft message to work on').'</h3></p>';
+  print '<p><h3>'.$I18N->get('Choose an existing draft message to work on').'</h3></p><br/>';
   $ls = new WebblerListing($I18N->get('Draft messages'));
   while ($row = Sql_Fetch_Array($req)) {
     $element = '<!--'.$row['id'].'-->'.$row['subject'];
