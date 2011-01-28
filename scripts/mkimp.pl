ba278b3b3dc4c088a1bcda7daefe87c2a0c1f62f
Michiel Dethmers <git@phplist.com>
2011-01-28 17:39:49 +0000

    small change

--- scripts/mkimp.pl
+++ scripts/mkimp.pl
@@ -3,15 +3,16 @@
 open F,">testimport.txt";
 print F "email\tName\tAddr1\tAddr2\tTown\tPostcode\tforeign key\tsend this user HTML emails\n";
 $start = 33300;
-for ($i=$start;$i<$start+10;$i++) {
+$num = 10000;
+for ($i=$start;$i<$start+$num;$i++) {
   print F "$i\@reallynonexistentdomain.com\tTestUser $i\tAddr line 1\tAddr line2\tTown\tPostcode $i\tABC $i\t1\n";
 }
 close(F);
 
 open F,">testimport2.txt";
 $start = 0;
-for ($i=$start;$i<$start+1000;$i++) {
-  print F "test$i\@dev.quipu\n";
+for ($i=$start;$i<$start+$num;$i++) {
+  print F "test$i\@phplist.local\n";
 }
 close(F);
 
