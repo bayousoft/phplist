7bb0a9cff73a2ac795955a01faa69e5bde79567c
Michiel Dethmers <git@phplist.com>
2011-04-19 13:41:45 +0000

    make it work in IE and fall back to redirect on failure

--- public_html/lists/js/phplist-subscribe-0.1.src.js
+++ public_html/lists/js/phplist-subscribe-0.1.src.js
@@ -1,17 +1,20 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
+
   $("#phplistsubscribeform").submit(function() {
     var emailaddress = $("#emailaddress").val();
     var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     var subscribeaddress = this.action;
-    subscribeaddress = subscribeaddress.replace(/asubscribe/,'subscribe');
+    ajaxaddress = subscribeaddress.replace(/subscribe/,'asubscribe');
+          
     if(emailReg.test(emailaddress)) {
       var jqxhr = $.ajax({
         type: 'POST',
-        url: this.action,
+        url: ajaxaddress,
+        crossDomain: true,
         data: "email="+emailaddress,
         success: function(data, textStatus, jqXHR ) {
-          if (data == 'FAIL') {
+          if (data.search(/FAIL/) >= 0) {
             document.location = subscribeaddress+"&email="+emailaddress;
           } else {
             $('#phplistsubscriberesult').html("<div id='subscribemessage'></div>");
@@ -44,6 +47,43 @@ $(document).ready(function(){
       $("#emailaddress").val(pleaseEnter)
     }
   });
+});
+
+// cross domain fix for IE
+// http://forum.jquery.com/topic/cross-domain-ajax-and-ie
+$.ajaxTransport("+*", function( options, originalOptions, jqXHR ) {
+  if(jQuery.browser.msie && window.XDomainRequest) {
+    var xdr;
+    return {
+        send: function( headers, completeCallback ) {
+            // Use Microsoft XDR
+            xdr = new XDomainRequest();
+            // would be nicer to keep it post
+            xdr.open("get", options.url+"&"+options.data);
+            xdr.onload = function() {
+                if(this.contentType.match(/\/xml/)){
+                    var dom = new ActiveXObject("Microsoft.XMLDOM");
+                    dom.async = false;
+                    dom.loadXML(this.responseText);
+                    completeCallback(200, "success", [dom]);
+                } else {
+                    completeCallback(200, "success", [this.responseText]);
+                }
+            };
+            xdr.ontimeout = function(){
+                completeCallback(408, "error", ["The request timed out."]);
+            };
+           
+            xdr.onerror = function(){
+                completeCallback(404, "error", ["The requested resource could not be found."]);
+            };
+            xdr.send();
+      },
+      abort: function() {
+          if(xdr)xdr.abort();
+      }
+    };
+  }
 });
 
 if (pleaseEnter == undefined) {
