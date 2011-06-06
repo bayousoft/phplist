7dcd23ca5ed143b1c62d4e4d399c6ae319c39426
Michiel Dethmers <git@phplist.com>
2011-06-06 15:36:48 +0000

    use phplist domain

--- scripts/mkimp.pl
+++ scripts/mkimp.pl
@@ -2,7 +2,7 @@
 # generate a test file to import
 open F,">testimport.txt";
 print F "email\tName\tAddr1\tAddr2\tTown\tPostcode\tforeign key\tsend this user HTML emails\n";
-$start = 33300;
+$start = 20;
 $num = 10000;
 for ($i=$start;$i<$start+$num;$i++) {
   print F "$i\@reallynonexistentdomain.com\tTestUser $i\tAddr line 1\tAddr line2\tTown\tPostcode $i\tABC $i\t1\n";
@@ -12,7 +12,7 @@ close(F);
 open F,">testimport2.txt";
 $start = 0;
 for ($i=$start;$i<$start+$num;$i++) {
-  print F "test$i\@phplist.local\n";
+  print F "testing-$i\@phplist.phplist.com\n";
 }
 close(F);
 
